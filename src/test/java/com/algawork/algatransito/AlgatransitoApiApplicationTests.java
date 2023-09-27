6496f1303a5c77d7156f71bf10abaf41cfd64ff9
manoeljr <manoelv8@gmail.com>
2023-09-27 20:20:37 -0300

    Start project AlgaWork

---
+++ src/test/java/com/algawork/algatransito/AlgatransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algawork.algatransito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgatransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
